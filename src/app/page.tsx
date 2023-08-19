8e95297ec6619c4716dfc2f91f57dadf82b77817
Joao Pedro <jaaopbr@gmail.com>
2023-08-19 12:29:09 -0300

    fix: minor adjusts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,19 +37,13 @@ export default function Home() {
       {/* Hero */}
       <section className="pb-4 h-screen w-full flex flex-col justify-center items-center text-zinc-950 dark:text-zinc-200">
         <div className="space-y-2 flex-1 flex flex-col justify-center">
-          <p className="text-3xl md:text-4xl lg:text-5xl font-semibold">Eaí,</p>
+          <p className="text-4xl md:text-5xl lg:text-6xl font-semibold">Eaí,</p>
           <div className="flex items-center justify-center gap-2">
-            {/* <span className="text-5xl md:text-7xl lg:text-9xl font-semibold dark:text-zinc-200 text-zinc-950">
-              &lt;
-            </span> */}
-            <h1 className="text-5xl md:text-7xl lg:text-9xl font-bold rounded p-2 bg-zinc-950 dark:bg-zinc-200 text-zinc-200 dark:text-zinc-950">
+            <h1 className="text-7xl md:text-8xl lg:text-9xl font-bold rounded p-2 bg-zinc-950 dark:bg-zinc-200 text-zinc-200 dark:text-zinc-950">
               Sou o João!
             </h1>
-            {/* <span className="tracking-[-6px] md:tracking-[-10px] lg:tracking-[-18px] text-5xl md:text-7xl lg:text-9xl font-semibold dark:text-zinc-200 text-zinc-950">
-              /&gt;
-            </span> */}
           </div>
-          <h4 className="text-md md:text-2xl lg:text-3xl text-zinc-950 dark:text-zinc-200 font-light">
+          <h4 className="text-lg md:text-3xl lg:text-4xl text-zinc-950 dark:text-zinc-200 font-light">
             <Typewriter
               words={[
                 'Desenvolvedor Front-End',
@@ -71,9 +65,9 @@ export default function Home() {
         <a
           href="#about"
           className="cursor-pointer -space-y-5 text-zinc-950 dark:text-zinc-200 opacity-60">
-          <ChevronDownIcon className="h-8 md:h-12 w-8 md:w-12 animate-bounce" />
-          <ChevronDownIcon className="h-8 md:h-12 w-8 md:w-12 animate-bounce animation-delay-100" />
-          <ChevronDownIcon className="h-8 md:h-12 w-8 md:w-12 animate-bounce animation-delay-150" />
+          <ChevronDownIcon className="h-12 md:h-12 w-12 md:w-12 animate-bounce" />
+          <ChevronDownIcon className="h-12 md:h-12 w-12 md:w-12 animate-bounce animation-delay-100" />
+          <ChevronDownIcon className="h-12 md:h-12 w-12 md:w-12 animate-bounce animation-delay-150" />
         </a>
       </section>
 
@@ -89,12 +83,11 @@ export default function Home() {
             com conhecimentos sólidos em diversas áreas.
           </h2>
 
-          <div className="w-full flex flex-col gap-8 items-center justify-center">
+          {/* <div className="w-full flex flex-col gap-8 items-center justify-center">
             <h2 className="text-2xl md:text-3xl lg:text-5xl">
               Algumas tecnologias que domino
             </h2>
 
-            {/* todo: Fazer 2+ sliders na vertical */}
             <div className="relative w-full md:max-w-5xl h-12 overflow-x-hidden">
               <div
                 style={{
@@ -131,18 +124,22 @@ export default function Home() {
                   ))}
               </div>
             </div>
+          </div> */}
 
-            <div className="w-full flex flex-col items-center justify-center gap-8 mt-12">
-              <h2 className="text-2xl md:text-4xl">
-                Estou disponível para novos Projetos
-              </h2>
-              <a
-                href="./cv_fullstack.pdf"
-                target="_blank"
-                className="max-w-xs text-center text-2xl p-2 rounded cursor-pointer bg-zinc-950 text-zinc-200 dark:bg-zinc-200 dark:text-zinc-950">
-                Baixe meu CV
-              </a>
-            </div>
+          <div className="w-full flex flex-col items-center justify-center gap-20">
+            <h3 className="text-3xl md:text-4xl lg:text-6xl">
+              Estou disponível para{' '}
+              <span className="px-2 rounded dark:bg-zinc-200 dark:text-zinc-950 bg-zinc-950 text-zinc-200">
+                novos Projetos
+              </span>
+              !
+            </h3>
+            <a
+              href="./cv_fullstack.pdf"
+              target="_blank"
+              className="w-full md:max-w-md text-center text-2xl p-2 rounded cursor-pointer bg-zinc-950 text-zinc-200 dark:bg-zinc-200 dark:text-zinc-950">
+              Baixe meu CV
+            </a>
           </div>
         </div>
       </section>
