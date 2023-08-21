7cc5bc7fd97245fb737c8b53f3f9a61953601d20
Joao Pedro <jaaopbr@gmail.com>
2023-08-21 15:55:51 -0300

    fix: minor adjusts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,38 +4,19 @@ import {
   LinkedInLogoIcon,
   ChevronDownIcon,
   DownloadIcon,
+  ArrowUpIcon,
 } from '@radix-ui/react-icons';
 import { Typewriter } from 'react-simple-typewriter';
 
-const techs: { name: string }[] = [
-  {
-    name: 'Angular',
-  },
-  {
-    name: 'Next',
-  },
-  {
-    name: 'Typescript',
-  },
-  {
-    name: 'Express',
-  },
-  {
-    name: 'Node',
-  },
-  {
-    name: 'Aws',
-  },
-  {
-    name: 'Postgres',
-  },
-];
+const date = new Date();
 
 export default function Home() {
   return (
     <main className="cursor-default relative flex flex-col items-center justify-center bg-gradient-to-br from-cyan-500 dark:from-indigo-700 dark:to-rose-900 to-teal-400">
       {/* Hero */}
-      <section className="pb-4 h-screen w-full flex flex-col justify-center items-center text-zinc-950 dark:text-zinc-200">
+      <section
+        id="hero"
+        className="pb-4 h-screen w-full flex flex-col justify-center items-center text-zinc-950 dark:text-zinc-200">
         <div className="space-y-2 flex-1 flex flex-col justify-center">
           <p className="text-4xl md:text-5xl lg:text-6xl font-semibold">Eaí,</p>
           <div className="flex items-center justify-center gap-2">
@@ -50,7 +31,7 @@ export default function Home() {
                 'Amante de Jogos',
                 'Desenvolvedor Back-End',
                 'Entusiasta de Novas Tecnologias',
-                'UI Designer',
+                'UI & UX Designer',
               ]}
               loop={false}
               cursor
@@ -83,49 +64,6 @@ export default function Home() {
             com conhecimentos sólidos em diversas áreas.
           </h2>
 
-          {/* <div className="w-full flex flex-col gap-8 items-center justify-center">
-            <h2 className="text-2xl md:text-3xl lg:text-5xl">
-              Algumas tecnologias que domino
-            </h2>
-
-            <div className="relative w-full md:max-w-5xl h-12 overflow-x-hidden">
-              <div
-                style={{
-                  left: '0%',
-                  top: '0%',
-                  width: 'inherit',
-                  height: 'inherit',
-                }}
-                className="absolute w-full flex items-center justify-around animate-primary text-zinc-200">
-                {techs &&
-                  techs.map((tech, index) => (
-                    <div
-                      key={index}
-                      className="py-1 px-2 text-center rounded text-md md:text-xl lg:text-2xl xl:text-3xl dark:text-zinc-950 text-zinc-200 dark:bg-zinc-200 bg-zinc-950">
-                      {tech.name}
-                    </div>
-                  ))}
-              </div>
-              <div
-                style={{
-                  left: '0%',
-                  top: '0%',
-                  width: 'inherit',
-                  height: 'inherit',
-                }}
-                className="absolute w-full flex items-center justify-around animate-secondary text-zinc-200">
-                {techs &&
-                  techs.map((tech, index) => (
-                    <div
-                      key={index}
-                      className="px-2 py-1 rounded text-md md:text-xl lg:text-2xl xl:text-3xl dark:text-zinc-950 text-zinc-200 dark:bg-zinc-200 bg-zinc-950">
-                      {tech.name}
-                    </div>
-                  ))}
-              </div>
-            </div>
-          </div> */}
-
           <div className="w-full flex flex-col items-center justify-center gap-20">
             <h3 className="text-3xl md:text-4xl lg:text-6xl">
               Estou disponível para{' '}
@@ -145,14 +83,16 @@ export default function Home() {
       </section>
 
       <footer className="h-24 w-full">
-        <div className="h-full px-20 flex items-center gap-4 text-zinc-950 dark:text-zinc-200">
-          <p className="text-2xl md:text-3xl">Feito por João Pedro</p>
-          <p className="text-2xl md:text-3xl">🚀</p>
+        <div className="h-full px-20 flex items-center justify-center gap-4 text-zinc-950 dark:text-zinc-200">
+          <small>
+            &copy; Copyright {date.getFullYear()}, JP Dev{' '}
+            <span className="ml-1">🚀</span>
+          </small>
         </div>
       </footer>
 
       {/* Floating Icons */}
-      <section className="z-50 m-8 flex gap-4 fixed right-0 bottom-0 text-zinc-950 dark:text-zinc-200">
+      <section className="z-50 m-6 flex gap-4 fixed right-0 bottom-0 text-zinc-950 dark:text-zinc-200">
         <a
           href="https://github.com/7cass"
           target="_blank"
